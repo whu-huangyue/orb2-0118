8e5209fd351a9c83dc3c9cf9d0f0bf045832e43c
huangyue <2967035281@qq.com>
2024-01-18 11:35:02 +0800

    bei_fen

---
+++ Examples/ROS/ORB_SLAM2/build/CMakeFiles/Stereo_new.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stereo_new.dir/src/ros_stereo_new.cc.o"
+  "../Stereo_new.pdb"
+  "../Stereo_new"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stereo_new.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Examples/ROS/ORB_SLAM2/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/hy/ORB_SLAM2/Examples/ROS/ORB_SLAM2/build/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/melodic/bin:/home/hy/anaconda3/bin:/home/hy/anaconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/cuda-11.0/bin'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/hy/ORB_SLAM2/Examples/ROS/ORB_SLAM2/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/home/hy/ORB_SLAM2/Examples/ROS/ORB_SLAM2:/home/hy/catkin_ws/src:/opt/ros/melodic/share'
